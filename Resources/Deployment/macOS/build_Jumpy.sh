0a1c5a0330ab4b282036f6f7ed6a06b1860beed7
Christian Ahrens <christianahrens@me.com>
2025-03-28 23:05:53 +0100

    Fixed provisioning profile UUID ref in macOS build script

--- Resources/Deployment/macOS/build_Jumpy.sh
+++ Resources/Deployment/macOS/build_Jumpy.sh
@@ -15,4 +15,4 @@ xcodebuild -project "$ProjucerPath"/Projucer.xcodeproj -configuration Release -j
 "$ProjucerBinPath" --resave "$JucerProjectPath"
 
 # start building the project. The provisioning profile specification refers to a profile manually created and physically present on the build machine...
-xcodebuild -project "$XCodeProjectPath" -configuration Release -jobs 8 PROVISIONING_PROFILE_SPECIFIER="7bf1cb91-9def-4bc2-ad75-e2b810b48fdd"
+xcodebuild -project "$XCodeProjectPath" -configuration Release -jobs 8 PROVISIONING_PROFILE_SPECIFIER="103a84c7-3d96-4f29-b7c7-18b4c63813d1"
